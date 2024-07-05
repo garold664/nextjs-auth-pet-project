0f7049abf65e3252b0b450de6a9f843a2358428f
garold664 <garold664@gmail.com>
2024-07-05 08:50:32 +0300

    adds links to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
       <h1>Home page</h1>
+      <Link href="/auth/register">Sign up</Link>
+      <Link href="/auth/login">Sign in</Link>
     </div>
   );
 }
