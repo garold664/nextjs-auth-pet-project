a172d3886f11bc3caee90c58a344390025435343
garold664 <garold664@gmail.com>
2024-07-05 12:47:36 +0300

    changes middleware.ts it is uses authConfig which is Edge compatible

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,16 @@
-import { auth } from './auth';
+import NextAuth from 'next-auth';
+
+import authConfig from './auth.config';
 // Use only one of the two middleware options below
 // 1. Use middleware directly
 // export const { auth: middleware } = NextAuth(authConfig)
 
 // 2. Wrapped middleware option
+
+const { auth } = NextAuth(authConfig);
 export default auth(async function middleware(req) {
   const isLoggedIn = !!req.auth;
+  // console.log('isLoggedIn: ', isLoggedIn);
 });
 
 export const config = {
