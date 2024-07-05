155f0a1c69d7ef7636d1cdd85966009da6f41a91
garold664 <garold664@gmail.com>
2024-07-05 19:01:52 +0300

    extends session with role field in session callback

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,15 +1,18 @@
 import { Button } from '@/components/ui/button';
 import { signOut } from '@/auth';
-
+import { auth } from '@/auth';
+import { json } from 'stream/consumers';
 async function handleSignOut() {
   'use server';
   await signOut();
 }
 
 export default async function SettingsPage() {
+  const session = await auth();
   return (
     <div>
       <h1>SettingsPage</h1>
+      <div>{JSON.stringify(session)}</div>
       <form action={handleSignOut}>
         <Button type="submit" variant={'link'}>
           Sign out
--- auth.ts
+++ auth.ts
@@ -7,12 +7,23 @@ import { db } from './lib/db';
 
 import authConfig from './auth.config';
 import { DEFAULT_LOGIN_REDIRECT } from './routes';
+import { getUserById } from './data/user';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   callbacks: {
+    async session({ session, token }) {
+      if (!token.sub) return session;
+
+      session.user.role = token.role;
+      return session;
+    },
     async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      token.role = existingUser.role;
       return token;
     },
     async redirect({ url, baseUrl }) {
