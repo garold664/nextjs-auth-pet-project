1c3ca074b6ccbb70c40a8644946ccca677b61ac4
garold664 <garold664@gmail.com>
2024-07-05 13:11:20 +0300

    implements routes protection in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
 import NextAuth from 'next-auth';
 
 import authConfig from './auth.config';
+import {
+  apiAuthPrefix,
+  authRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+  publicRoutes,
+} from './routes';
 // Use only one of the two middleware options below
 // 1. Use middleware directly
 // export const { auth: middleware } = NextAuth(authConfig)
@@ -10,7 +16,28 @@ import authConfig from './auth.config';
 const { auth } = NextAuth(authConfig);
 export default auth(async function middleware(req) {
   const isLoggedIn = !!req.auth;
-  // console.log('isLoggedIn: ', isLoggedIn);
+  const { nextUrl } = req;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return undefined;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return undefined;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL('/auth/login', nextUrl));
+  }
+
+  return undefined;
 });
 
 export const config = {
